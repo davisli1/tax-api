226466d254c6a02f054457c70daa2d0aae10b2d7
Davis Li <davismxli@gmail.com>
2024-05-31 00:50:55 -0400

    integrated provincial + federal

--- api.py
+++ api.py
@@ -2,28 +2,37 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# Define tax brackets and rates for British Columbia (BC)
-tax_brackets = [10, 20]  # Example tax brackets
-tax_rates = [0.05, 0.07]  # Example tax rates
+# Define federal tax brackets and rates
+federal_tax_brackets = [55867, 111733, 173205, 246752]
+federal_tax_rates = [0.15, 0.205, 0.26, 0.29, 0.33]
 
-def calculate_tax(income):
+# Define provincial tax brackets and rates for Ontario and British Columbia
+provincial_tax_data = {
+    'ON': {
+        'brackets': [51446, 102894, 150000, 220000],
+        'rates': [0.0505, 0.0915, 0.1116, 0.1216, 0.1316]
+    },
+    'BC': {
+        'brackets': [47937, 95875, 110076, 133664, 181232, 252752],
+        'rates': [0.0506, 0.077, 0.105, 0.1229, 0.147, 0.168, 0.205]
+    }
+}
+
+def calculate_tax(income, brackets, rates):
     tax = 0
     remaining_income = income
 
-    for i in range(len(tax_brackets)):
-        bracket_limit = tax_brackets[i]
-        rate = tax_rates[i]
+    for i in range(len(brackets)):
+        bracket_limit = brackets[i]
+        rate = rates[i]
 
         if remaining_income > bracket_limit:
-            tax += bracket_limit * rate
-            remaining_income -= bracket_limit
+            tax += (bracket_limit - (brackets[i-1] if i > 0 else 0)) * rate
+            remaining_income -= (bracket_limit - (brackets[i-1] if i > 0 else 0))
         else:
             tax += remaining_income * rate
             remaining_income = 0
             break
-
-    if remaining_income > 0:
-        tax += remaining_income * tax_rates[-1]
 
     return tax
 
@@ -31,10 +40,22 @@ def calculate_tax(income):
 def calculate_tax_break():
     net_income_before_expense = float(request.args.get('netIncomeBeforeExpense'))
     net_income_after_expense = float(request.args.get('netIncomeAfterExpense'))
+    province = request.args.get('province')
 
-    tax_before = calculate_tax(net_income_before_expense)
-    tax_after = calculate_tax(net_income_after_expense)
-    tax_break = tax_before - tax_after
+    if province not in provincial_tax_data:
+        return jsonify({'error': 'Invalid province provided'}), 400
+
+    federal_tax_before = calculate_tax(net_income_before_expense, federal_tax_brackets, federal_tax_rates)
+    provincial_brackets = provincial_tax_data[province]['brackets']
+    provincial_rates = provincial_tax_data[province]['rates']
+    provincial_tax_before = calculate_tax(net_income_before_expense, provincial_brackets, provincial_rates)
+    total_tax_before = federal_tax_before + provincial_tax_before
+
+    federal_tax_after = calculate_tax(net_income_after_expense, federal_tax_brackets, federal_tax_rates)
+    provincial_tax_after = calculate_tax(net_income_after_expense, provincial_brackets, provincial_rates)
+    total_tax_after = federal_tax_after + provincial_tax_after
+
+    tax_break = total_tax_before - total_tax_after
 
     return jsonify({
         'taxBreak': round(tax_break, 2),
